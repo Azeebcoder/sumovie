ee1b542e0dbfbd35b648644f8bcaf5c750318a5d
Azeeb_coder <djadamnoob9568@gmail.com>
2025-01-21 13:04:33 +0530

    Update Cast.jsx

--- src/components/cast/Cast.jsx
+++ src/components/cast/Cast.jsx
@@ -21,7 +21,7 @@ const Cast = ({ id }) => {
           credits.map((credit, index) => (
             <div key={credit.id} className={styles.cast}>
               <img
-                src={credit.profile_path? `https://image.tmdb.org/t/p/w200${credit.profile_path}`:"https://victoriastrokerecovery.org/wp-content/uploads/2024/07/placeholder.jpg"}
+                src={credit.profile_path? `https://image.tmdb.org/t/p/w200${credit.profile_path}`:"https://cdn.pixabay.com/photo/2018/11/13/21/43/avatar-3814049_1280.png"}
                 alt="User"
               />
               <h3>{credit.name}</h3>
